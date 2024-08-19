c454f6a0c768aa33dfe65328a862b2fca6d4a0f6
Zakaria Madaoui <zakarias.madaoui@katim.com>
2024-08-19 15:44:44 +0300

    fix run as root

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = b709bba6910f036221b45409b4d80f38589d82c3
+AESD_ASSIGNMENTS_VERSION = bb2856bbadc677bbe754a53a0454e8521b2ad6c5
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
